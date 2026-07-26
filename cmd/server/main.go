41a56b76ddf4162fc5d48b41794155d80fe01b89
Roman4k-gg <sendelroma@gmail.com>
2026-07-27 01:20:45 +0600

    make basic listener

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main(){
+	listener,err :=net.Listen("tcp",":6379")
+	if err != nil{
+		panic(err)
+	}
+	defer listener.Close()
+	fmt.Println("Server is running on port :6379")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn){
+
+	defer conn.Close()
+    fmt.Println("New client connected!")
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Roman4k-gg/My-Own-Redis
+
+go 1.24.6
